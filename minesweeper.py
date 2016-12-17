d8d77adfeebf573183cef5e6c5eac3c627b62dc8
David Becerra <dbecerra@usc.edu>
2016-12-16 17:52:28 -0800

    Kivy-ify

--- minesweeper.py
+++ minesweeper.py
@@ -4,7 +4,7 @@ import kivy
 kivy.require('1.9.1')
 
 from kivy.app import App
-
+from kivy.uix.widget import Widget
 
 import argparse
 
@@ -15,19 +15,24 @@ class MineSweeperError(Exception):
     pass
 
 
-class Minesweeper(object):
-    def __init__(self, difficulty='easy'):
-        if difficulty == 'easy':
-            grid_params = {'nrows': 8, 'ncols': 8, 'nmines': 10}
-        elif difficulty == 'medium':
-            grid_params = {'nrows': 16, 'ncols': 16, 'nmines': 40}
-        elif difficulty == 'hard':
-            grid_params = {'nrows': 24, 'ncols': 24, 'nmines': 99}
-        else:
-            raise MineSweeperError('Unsupported difficulty setting: %s' % difficulty)
-        self.game_board = Grid(**grid_params)
-        self.game_board.print_grid()
+class Minesweeper(Widget):
+    pass
+    #def __init__(self, difficulty='easy'):
+        #if difficulty == 'easy':
+        #    grid_params = {'nrows': 8, 'ncols': 8, 'nmines': 10}
+        #elif difficulty == 'medium':
+        #    grid_params = {'nrows': 16, 'ncols': 16, 'nmines': 40}
+        #elif difficulty == 'hard':
+        #    grid_params = {'nrows': 24, 'ncols': 24, 'nmines': 99}
+        #else:
+        #    raise MineSweeperError('Unsupported difficulty setting: %s' % difficulty)
+        #self.game_board = Grid(**grid_params)
+        #self.game_board.print_grid()
 
+
+class MinesweeperApp(App):
+    def build(self):
+        return Minesweeper()
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -37,4 +42,5 @@ def parse_args():
 if __name__ == '__main__':
     print('Starting game...')
     args = parse_args()
-    MS = Minesweeper(difficulty=args.difficulty)
+    MinesweeperApp().run()
+    #MS = Minesweeper(difficulty=args.difficulty)
