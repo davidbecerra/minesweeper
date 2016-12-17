e1c440485ae3017e9fab9930720dee9478576098
David Becerra <dbecerra@usc.edu>
2016-12-16 16:49:23 -0800

    Redo

--- grid.py
+++ grid.py
@@ -1,6 +1,57 @@
+from __future__ import print_function
+
+import numpy as np
+from itertools import product
+
 
 class Grid(object):
-	"""docstring for Grid"""
-	def __init__(self, arg):
-		super(Grid, self).__init__()
-		self.arg = arg
+    def __init__(self, nrows=8, ncols=8, nmines=2):
+        self.nrows = nrows
+        self.ncols = ncols
+        self.nmines = nmines
+        self.grid = np.array([[Cell() for _ in xrange(ncols)] for _ in xrange(nrows)], dtype=object)
+        self.populate_mines()
+
+    def populate_mines(self):
+        all_positions = list(product(xrange(self.nrows), xrange(self.ncols)))
+        mine_indices = np.random.choice(xrange(self.nrows * self.ncols), size=self.nmines, replace=False)
+        for mi in mine_indices:
+            gindex = all_positions[mi]
+            self.grid[gindex].value = -1
+            self.populate_neighbors(gindex)
+
+    def populate_neighbors(self, gindex):
+        for dx, dy in product([-1, 0, 1], repeat=2):
+            ix = gindex[0] + dx
+            iy = gindex[1] + dy
+            # Don't change gindex (dx and dy are both zero)
+            # Out of bounds
+            # Don't change a mine square
+            if dx == dy == 0 or \
+               ix < 0 or ix >= self.nrows or iy < 0 or iy >= self.ncols or \
+               self.grid[ix, iy].is_mine():
+                continue
+            else:
+                self.grid[ix, iy] += 1
+
+    def print_grid(self):
+        print('Game Board\n----------------------')
+        for row in self.grid:
+            print('|', end="")
+            for cell in row:
+                if cell.is_mine():
+                    print('| X ', end="")
+                else:
+                    print('| %d ' % cell.value, end="")
+            print('|')
+
+class Cell(object):
+    def __init__(self, value=0):
+        self.value = value
+
+    def is_mine(self):
+        return self.value == -1
+
+    def __iadd__(self, delta):
+        self.value += delta
+        return self
--- minesweeper.py
+++ minesweeper.py
@@ -1,88 +1,40 @@
-import game
+from __future__ import print_function
 
-import random
-import time
+import kivy
+kivy.require('1.9.1')
 
-class MinesweeperGame(object):
-	"""
-	Class for a Minesweeper game template.
-	Hopefully this class is flexible enough to be implemented as various forms
-	of Minesweeper (i.e. certain configurations or mines or stuff).
-	"""
-	def __init__(self, nMines, columns, rows):
-		# super(MinesweeperGame, self).__init__()
-		self.nMines = nMines
-		self.columns = columns
-		self.rows = rows
-		self.board = []
-		# try:
-		self.init_board()
-		# except Exception, e:
-			# print "Caught error in board initialization:"
-			# raise e
-
-	def print_board(self, src = 'log.txt'):
-		f = open(src, 'w')
-		for row in xrange(self.rows):
-			for col in xrange(self.columns):
-				f.write(str(self.board[row][col]))
-				f.write(" | ")
-			f.write("\n")
-		f.close()
-
-	def init_mines(self):
-		'''
-		Randomly chooses a grid position to place each mine. 
-		'''
-		start = time.clock()
-		for dummy in xrange(self.nMines):
-			while True:
-				row,column = random.randint(0, self.rows - 1), random.randint(0, self.columns - 1)
-				end = time.clock()
-				if self.board[row][column] != -1:
-					self.board[row][column] = -1
-					break
-				if start - end >= 10.0:
-					raise Error("Mine initialize error: Timeout failure.")
-					print	"Test in game.py init_mines method"
-					break
-			start = end
-
-	def get_adjacent_mines(self, row, col):
-		adj_mines = 0
-		for row_i in xrange(row - 1, row + 2):
-			for col_i in xrange(col - 1, col + 2):
-				if (row_i != row or col_i != col) and row_i >= 0 and row_i < self.rows and col_i >= 0 and col_i	< self.columns:
-					if self.board[row_i][col_i] is -1:
-						adj_mines += 1
-		return adj_mines
-
-	def set_numbers(self):
-		for row in xrange(self.rows):
-			for col in xrange(self.columns):
-				if self.board[row][col] is not -1:
-					self.board[row][col] = self.get_adjacent_mines(row, col)
-
-	def init_board(self):
-		# Sets board to a row by column grid of zeros
-		for row in xrange(self.rows):
-			self.board.append([0] * self.columns)
-		self.init_mines()
-		self.print_board()
-		self.set_numbers()
-		self.print_board()
+from kivy.app import App
 
 
-	def is_Goal(self):
-		"""
-		Checks to see if current board configuration is the goal configuration 
-		(i.e. all non-mine squares are clicked)
-		"""
-		pass
+import argparse
 
-def main():
-	print "Welcome to Minesweeper"
-	game = MinesweeperGame(2,4, 4)
+from grid import Grid
+
+
+class MineSweeperError(Exception):
+    pass
+
+
+class Minesweeper(object):
+    def __init__(self, difficulty='easy'):
+        if difficulty == 'easy':
+            grid_params = {'nrows': 8, 'ncols': 8, 'nmines': 10}
+        elif difficulty == 'medium':
+            grid_params = {'nrows': 16, 'ncols': 16, 'nmines': 40}
+        elif difficulty == 'hard':
+            grid_params = {'nrows': 24, 'ncols': 24, 'nmines': 99}
+        else:
+            raise MineSweeperError('Unsupported difficulty setting: %s' % difficulty)
+        self.game_board = Grid(**grid_params)
+        self.game_board.print_grid()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--difficulty', choices=['easy', 'medium', 'hard'], default='easy')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-	main()
+    print('Starting game...')
+    args = parse_args()
+    MS = Minesweeper(difficulty=args.difficulty)
